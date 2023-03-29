716e7e64361232edeac2599a452322f04cb881c1
Ivanushka13 <isdavydov_3@edu.hse.ru>
2023-03-29 15:53:24 +0300

    test

--- web/src/Components/TestAPI.jsx
+++ web/src/Components/TestAPI.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, {Component} from "react";
 import Axios from 'axios';
 
 export default class TestAPI extends Component {
@@ -8,39 +8,32 @@ export default class TestAPI extends Component {
             items: [],
         }
     }
-     config = {
-        url: "https://localhost:7153/Users/byId/1",
-        method: "get",
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Credentials': 'true'
-        }
-    };
+
     componentDidMount() {
-        // Axios.get("https://localhost:7153/Users/byId/1", this.config)
-        //     .then(
-        //         (json) => {
-        //             console.log(json.data);
-        //         // this.setState({
-        //         //     items: json.data.drinks
-        //         // });
-        //     })
-        let response = Axios.request(this.config);
-        console.log(response);
+        Axios.get(`https://localhost:7153/Users/byId?id=1`)
+            .then(
+                (json) => {
+                    this.setState({
+                        items: json.data
+                    });
+                })
     }
 
     render() {
-        //const { items } = this.state;
-
+        const {items} = this.state
         return (
-            <p> TEST </p>
+
+            <p> {items.name} </p>
+
         )
     }
 
 }
 
 
-{/*<button onClick={getJoke}> Press to impress) </button>*/}
-{/*{items.map(item => {*/}
-{/*})}*/}
+{/*<button onClick={getJoke}> Press to impress) </button>*/
+}
+{/*{items.map(item => {*/
+}
+{/*})}*/
+}

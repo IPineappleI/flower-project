2df42325bb18236a8a79b2993a09c74f48762a28
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2023-03-29 16:16:04 +0300

    Added current mobile app version

---
+++ mobileApp/styles/globalStyle.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native";
+
+export const globalStyle = StyleSheet.create({
+    main: {
+        flex: 1,
+        padding: 20
+    },
+    title: {
+        fontSize: 20,
+        color: '#333',
+        fontFamily: 'os-bold'
+    }
+});

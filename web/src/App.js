b44ea86162beb3ddeaef21e17503c5eb1e7453cb
Ivanushka13 <isdavydov_3@edu.hse.ru>
2023-03-29 13:44:01 +0300

    test API

--- web/src/App.js
+++ web/src/App.js
@@ -6,26 +6,29 @@ import UsersList from "./Pages/UsersList/UsersList";
 import ProductsList from "./Pages/ProductsList/ProductsList";
 import SingleUser from "./Pages/SingleUser/SingleUser";
 import NewUser from "./Pages/NewUser/NewUser";
+import React, { useState } from "react";
+import TestAPI from "./Components/TestAPI";
 
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/">
-                        <Route index element={<Home/>}/>
-                        <Route path="login" element={<LoginPage/>}/>
-                        <Route path="orders" element={<OrdersList/>}/>
-                        <Route path="users">
-                            <Route index element={<UsersList/>}/>
-                            <Route path=":userId" element={<SingleUser/>}/>
-                            <Route path="new" element={<NewUser/>}></Route>
-                        </Route>
-                        <Route path="products" element={<ProductsList/>}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <TestAPI />
+        // <div className="App">
+        //     <BrowserRouter>
+        //         <Routes>
+        //             <Route path="/">
+        //                 <Route index element={<Home/>}/>
+        //                 <Route path="login" element={<LoginPage/>}/>
+        //                 <Route path="orders" element={<OrdersList/>}/>
+        //                 <Route path="users">
+        //                     <Route index element={<UsersList/>}/>
+        //                     <Route path=":userId" element={<SingleUser/>}/>
+        //                     <Route path="new" element={<NewUser/>}></Route>
+        //                 </Route>
+        //                 <Route path="products" element={<ProductsList/>}/>
+        //             </Route>
+        //         </Routes>
+        //     </BrowserRouter>
+        // </div>
     )
 }
 
---
+++ web/src/Components/TestAPI.jsx
@@ -0,0 +1,36 @@
+import React, { Component, useState } from "react";
+import Axios from 'axios';
+
+export default class TestAPI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+        }
+    }
+
+    componentDidMount() {
+        Axios.get("https://localhost:7153")
+            .then(
+                (json) => {
+                    console.log(json.data);
+                // this.setState({
+                //     items: json.data.drinks
+                // });
+            })
+    }
+
+    render() {
+        //const { items } = this.state;
+
+        return (
+            <p> TEST </p>
+        )
+    }
+
+}
+
+
+{/*<button onClick={getJoke}> Press to impress) </button>*/}
+{/*{items.map(item => {*/}
+{/*})}*/}

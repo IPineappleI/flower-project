46d2417332494c7d6c2490a5f42720fb9273d9e5
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2023-03-29 19:17:09 +0300

    Fixed SplashScreen

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -4,51 +4,50 @@ import {globalStyle} from "./styles/globalStyle";
 import Header from "./components/Header";
 import React, {useCallback, useEffect, useState} from 'react';
 import {
-  Text,
-  SafeAreaView,
-  View
+    Text,
+    SafeAreaView,
+    View
 } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
+    const [appIsReady, setAppIsReady] = useState(false);
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Preload fonts, making any API calls
-        await Font.loadAsync({
-          'os-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-          'os-bold-it': require('./assets/fonts/OpenSans-BoldItalic.ttf'),
-          'os-light': require('./assets/fonts/OpenSans-Light.ttf'),
-          'os-light-it': require('./assets/fonts/OpenSans-LightItalic.ttf')
-        });
-        console.log("fonts loaded");
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // Preload fonts, making any API calls
+                await Font.loadAsync({
+                    'os-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+                    'os-bold-it': require('./assets/fonts/OpenSans-BoldItalic.ttf'),
+                    'os-light': require('./assets/fonts/OpenSans-Light.ttf'),
+                    'os-light-it': require('./assets/fonts/OpenSans-LightItalic.ttf')
+                });
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    if (!appIsReady) {
+        return null;
     }
 
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  return (
-      <SafeAreaView>
-        <Text style={globalStyle.title}>Hello world!</Text>
-      </SafeAreaView>
-  );
+    return (
+        <SafeAreaView onLayout={onLayoutRootView}>
+            <Text style={globalStyle.title}>Hello world!</Text>
+        </SafeAreaView>
+    );
 }

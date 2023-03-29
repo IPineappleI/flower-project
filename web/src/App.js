c8385154af5c8ab1f6f00b6676d45240aecd6170
Ivanushka13 <isdavydov_3@edu.hse.ru>
2023-03-29 16:47:05 +0300

    test UsersDatatable

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import LoginPage from "./Pages/Login/LoginPage";
 import OrdersList from "./Pages/OrdersList/OrdersList"
@@ -6,29 +6,28 @@ import UsersList from "./Pages/UsersList/UsersList";
 import ProductsList from "./Pages/ProductsList/ProductsList";
 import SingleUser from "./Pages/SingleUser/SingleUser";
 import NewUser from "./Pages/NewUser/NewUser";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import TestAPI from "./Components/TestAPI";
 
 function App() {
     return (
-        <TestAPI />
-        // <div className="App">
-        //     <BrowserRouter>
-        //         <Routes>
-        //             <Route path="/">
-        //                 <Route index element={<Home/>}/>
-        //                 <Route path="login" element={<LoginPage/>}/>
-        //                 <Route path="orders" element={<OrdersList/>}/>
-        //                 <Route path="users">
-        //                     <Route index element={<UsersList/>}/>
-        //                     <Route path=":userId" element={<SingleUser/>}/>
-        //                     <Route path="new" element={<NewUser/>}></Route>
-        //                 </Route>
-        //                 <Route path="products" element={<ProductsList/>}/>
-        //             </Route>
-        //         </Routes>
-        //     </BrowserRouter>
-        // </div>
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Home/>}/>
+                        <Route path="login" element={<LoginPage/>}/>
+                        <Route path="orders" element={<OrdersList/>}/>
+                        <Route path="users">
+                            <Route index element={<UsersList/>}/>
+                            <Route path=":userId" element={<SingleUser/>}/>
+                            <Route path="new" element={<NewUser/>}></Route>
+                        </Route>
+                        <Route path="products" element={<ProductsList/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
